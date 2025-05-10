6dc63da3f46ce2b49d7276e315057588df2ed734
Matias Aduco <matias.aduco@gmail.com>
2025-05-09 22:43:31 -0300

    Implementado React YouTube

--- src/components/MusicPlayer.tsx
+++ src/components/MusicPlayer.tsx
@@ -1,38 +1,32 @@
 import { Skeleton, TextField } from '@mui/material'
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
+import YouTube from 'react-youtube'
 
 const MusicPlayer = () => {
-  const [url, setUrl] = useState('')
+  const [videoId, setVideoId] = useState<string>('')
 
-  const transformUrl = (input: string) => {
-    const match = input.match(/v=([\w-]+)/)
-    return match ? `https://www.youtube.com/embed/${match[1]}` : ''
+  const handleChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
+    const url = new URL(target.value)
+    const videoId = url.searchParams.get('v') ?? ''
+    setVideoId(videoId)
   }
 
   return (
-    <div>
+    <div className='flex flex-col items-center w-full shadow-lg p-5 pb-10'>
       <h1 className='text-4xl mb-2 text-center'>Music Player</h1>
 
       <TextField
         id='name'
+        style={{ width: '560px' }}
         margin='dense'
         name='name'
         label='YouTube link...'
         type='text'
-        onChange={(e) => setUrl(transformUrl(e.target.value))}
-        fullWidth
+        onChange={handleChange}
       />
 
-      {url ? (
-        <iframe
-          width='560'
-          height='315'
-          src={url}
-          title='YouTube video player'
-          frameBorder='0'
-          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-          allowFullScreen
-        />
+      {videoId ? (
+        <YouTube videoId={videoId} opts={{ width: '560' }} />
       ) : (
         <Skeleton variant='rectangular' width={560} height={315} />
       )}
